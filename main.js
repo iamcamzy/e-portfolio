76b5755bc2ba1e8ccd2c221c65f1e024b65a5bbb
iamCamzy <jrgaloriodev04@gmail.com>
2025-01-26 08:07:02 +0800

    Few tweaks

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*================================= toggle icon navbar ===================================*/
+// Icon Navbar
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -7,7 +7,7 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active')
 };
 
-/*================================= scroll section active link ===================================*/
+// Scroll
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -26,41 +26,34 @@ window.onscroll = () => {
         };
     });
 
-    /*================================= sticky navbar ===================================*/
+// Navbar Sticky
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 
-    /*================================= remove toggle icon and navbar ===================================*/
+// Toggle icon navbar
     menuIcon.classList.remove('fa-xmark');
     navbar.classList.remove('active');
 };
 
-/*================================= scroll reveal ===================================*/
+// ScrollReveal
 ScrollReveal({
     //reset: true,
     distance: '80px',
     duration: 2000,
-    delay: 200,
+    delay: 80,
 });
 
 ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-img, .skills-container, .projects-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+ScrollReveal().reveal('.icons', { origin: 'right'});
+ScrollReveal().reveal('.skills', { origin: 'top'});
+ScrollReveal().reveal('.projects', { origin: 'top'});
 
-
-/*================================= typed js ===================================*/
-
+// Typed JS
 const typed = new Typed('.multiple-text', {
     strings: ['Backend Developer', 'Backend Developer', 'Backend Developer'],
-    typeSpeed: 70,
-    backSpeed: 70,
-    backDelay: 1000,
-    loop: true,
-});
-
-const numType = new Typed('.multiple-texts', {
-    strings: ['Contact Me!', 'Contact Me!', 'Contact Me!'],
     typeSpeed: 70,
     backSpeed: 70,
     backDelay: 1000,
